af2fdfab180aa5c3431794e145e78b76c809bfe1
Alfred Meshcheryakov <ameshcheryakov@plazius.ru>
2019-08-21 20:51:48 +0300

    Lesson-5

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', mainapp.main, name='main'),
-    path('products/', mainapp.products, name='products'),
+    path('products/', include('mainapp.urls', namespace='products')),
+    path('basket/', include('basketapp.urls', namespace='basket')),
     path('contact/', mainapp.contacts, name='contacts'),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('admin/', admin.site.urls),
